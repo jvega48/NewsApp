b56e72d8dd637a2613da849e69af926666918882
Jose Vega <jvega48@calstatela.edu>
2017-06-29 23:55:34 -0700

    Final Version Working
    Look at the readme file.

--- NewsApp/app/src/main/java/com/example/jose/newsapp/MainActivity.java
+++ NewsApp/app/src/main/java/com/example/jose/newsapp/MainActivity.java
@@ -1,98 +1,156 @@
 package com.example.jose.newsapp;
 
-import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.nfc.Tag;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.example.jose.newsapp.utilities.NetworkUtils;
+import com.example.jose.newsapp.utilities.NewsAdapter;
+import com.example.jose.newsapp.utilities.NewsItem;
+import com.example.jose.newsapp.utilities.NewsJSONUtils;
+
+import org.json.JSONException;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Scanner;
+import java.security.Key;
+import java.util.ArrayList;
+
+
 
 public class MainActivity extends AppCompatActivity {
-    private EditText nSearchBoxEditText;
-    private TextView nURLDisplayTextView;
-    private TextView nSearchResultsTextView;
-    private  TextView errorMessage;
+    public static String SOURCE = "techcrunch";
+    public static String SORTBY = "latest";
+    static  final String TAG = "MainActivity";
+    //private EditText nSearchBoxEditText;
+    //private TextView nURLDisplayTextView;
+    //private TextView nSearchResultsTextView;
+    //private  TextView errorMessage;
     private ProgressBar progressBar;
+    private RecyclerView recyclerView;
+    private NewsAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        nSearchBoxEditText = (EditText) findViewById(R.id.et_search_box);
-        nURLDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
-        nSearchResultsTextView = (TextView) findViewById(R.id.tv_newssearch_result_json);
-        errorMessage = (TextView) findViewById(R.id.tv_error_message_display);
+        //nSearchBoxEditText = (EditText) findViewById(R.id.et_search_box);
+        //nURLDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
+        //nSearchResultsTextView = (TextView) findViewById(R.id.tv_newssearch_result_json);
+        //errorMessage = (TextView) findViewById(R.id.tv_error_message_display);
         progressBar = (ProgressBar) findViewById(R.id.pb_loading_indicator);
+        recyclerView =(RecyclerView) findViewById(R.id.recycle_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        NetworkTask task = new NetworkTask(SOURCE,SORTBY,getString(R.string.key));
+        task.execute();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu,menu);
-        return true;
-    }
+ //   @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu,menu);
+//        return true;
+//    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int itemChoose = item.getItemId();
-        if(itemChoose == R.id.action_search){
-            makeNewsSearchQuery();
-            return true;
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int itemChoose = item.getItemId();
+//        if(itemChoose == R.id.action_search){
+//            //makeNewsSearchQuery();
+//            NetworkTask task = new NetworkTask(SOURCE,SORTBY,getString(R.string.key));
+//            task.execute();
+//        }
+//        return true;
+//    }
+
+//    public void showNewsJsonData(){
+//        nSearchResultsTextView.setVisibility(View.VISIBLE);
+//        errorMessage.setVisibility(View.INVISIBLE);
+//    }
+
+//    public void showError(){
+//        nSearchResultsTextView.setVisibility(View.INVISIBLE);
+//        errorMessage.setVisibility(View.VISIBLE);
+//    }
+
+//    private void makeNewsSearchQuery(){
+//        String newsQuery = nSearchBoxEditText.getText().toString();
+//        URL newsSearchUrl = NetworkUtils.buildURL(newsQuery);
+//        //nURLDisplayTextView.setText(newsSearchUrl.toString());
+//        new newsQueryRask().execute(newsSearchUrl);
+//    }
+
+    public class NetworkTask extends AsyncTask<URL,Void,ArrayList<NewsItem>>{
+        String source,sort,key;
+
+        NetworkTask(String SOURCE,String SORTBY,String KEY){
+            SOURCE = source;
+            SORTBY = sort;
+            KEY = key;
         }
-        return super.onOptionsItemSelected(item);
+
+        public void showNewsJsonData(){
+            recyclerView.setVisibility(View.VISIBLE);
+            //errorMessage.setVisibility(View.INVISIBLE);
     }
 
-    public void showNewsJsonData(){
-        nSearchResultsTextView.setVisibility(View.VISIBLE);
-        errorMessage.setVisibility(View.INVISIBLE);
+        public void showError(){
+        recyclerView.setVisibility(View.INVISIBLE);
+        //errorMessage.setVisibility(View.VISIBLE);
     }
-
-    public void showError(){
-        nSearchResultsTextView.setVisibility(View.INVISIBLE);
-        errorMessage.setVisibility(View.VISIBLE);
-    }
-
-    private void makeNewsSearchQuery(){
-        String newsQuery = nSearchBoxEditText.getText().toString();
-        URL newsSearchUrl = NetworkUtils.buildURL(newsQuery);
-        nURLDisplayTextView.setText(newsSearchUrl.toString());
-        new newsQueryRask().execute(newsSearchUrl);
-    }
-    public class newsQueryRask extends AsyncTask<URL,Void,String>{
         @Override
-        protected String doInBackground(URL... params) {
-            URL lookingURL = params[0];
-            String newsSearchRslt = null;
+        protected ArrayList<NewsItem> doInBackground(URL... params) {
+//            URL lookingURL = params[0];
+            String json = null;
+            ArrayList<NewsItem> results = null;
+            URL url = NetworkUtils.buildURL(SOURCE,SORTBY,getString(R.string.key));
+            Log.d(TAG, "URL: " + url.toString());
             try {
 
-                newsSearchRslt = NetworkUtils.getResponsedFromHttpUrl(lookingURL);
-
-            }catch (IOException e){
+                json = NetworkUtils.getResponseFromHttpUrl(url);
+                results = NewsJSONUtils.parseJSON(json);
+            }catch (Exception e){
                 e.printStackTrace();
             }
-            return newsSearchRslt;
+            return results;
         }
 
         @Override
-        protected void onPostExecute(String newSearchRslt) {
-            progressBar.setVisibility(View.INVISIBLE);
-            if(newSearchRslt != null && !newSearchRslt.equals("")){
+        protected void onPostExecute(final ArrayList<NewsItem> data) {
+            super.onPostExecute(data);
+            progressBar.setVisibility(View.GONE);
+            if( data != null){
+                adapter = new NewsAdapter(data, new NewsAdapter.ItemClickListener(){
+
+                    @Override
+                    public void onItemClick(int clickedItemIndex) {
+                                String url = data.get(clickedItemIndex).getUrl();
+                                Log.d(TAG, String.format("URL %s", url));
+                                openNewWebPage(url);
+                    }
+                });
+                recyclerView.setAdapter(adapter);
                 showNewsJsonData();
-                nSearchResultsTextView.setText(newSearchRslt);
-            }else{
+            }else {
                 showError();
+            }
+
+        }
+        public void openNewWebPage(String params){
+            Uri site = Uri.parse(params);
+            Intent intent = new Intent(Intent.ACTION_VIEW, site);
+            if(intent.resolveActivity(getPackageManager()) != null){
+                startActivity(intent);
             }
         }
 
--- NewsApp/app/src/main/java/com/example/jose/newsapp/utilities/NetworkUtils.java
+++ NewsApp/app/src/main/java/com/example/jose/newsapp/utilities/NetworkUtils.java
@@ -1,8 +1,11 @@
 package com.example.jose.newsapp.utilities;
 
+import android.net.Credentials;
 import android.net.Uri;
+import android.util.Log;
 
 import com.example.jose.newsapp.MainActivity;
+import com.example.jose.newsapp.R;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,17 +18,23 @@ import java.util.Scanner;
  * Created by Jose on 6/15/2017.
  */
 
-public class NetworkUtils extends MainActivity {
+public class NetworkUtils {
 
 	//Add your unique API key for it to work, removed mine for security purposes
-    final static String APIurl = "https://newsapi.org/v1/articles?source=techcrunch&sortBy=" +
-            "top&apiKey=";
-
-    public static URL buildURL(String newsSearchQuery){
-        Uri buildUri = Uri.parse(APIurl).buildUpon().build();
+    //final static String APIurl = "https://newsapi.org/v1/articles?source=techcrunch&sortBy=" +
+           // "top&apiKey=";
+    public static final String TAG ="NetworkUtils";
+    public static final String MAIN_URL = "https://newsapi.org/v1/articles";
+    public static final String PARAM_SOURCE ="source";
+    public static final String PARAM_SORT_BY ="sortBy";
+    public  static final String PARAM_KEY= "apiKey";
+    public static URL buildURL(String source, String sortBy, String key){
+        Uri buildUri = Uri.parse(MAIN_URL).buildUpon().appendQueryParameter(PARAM_SOURCE, source).appendQueryParameter(PARAM_SORT_BY, sortBy).
+                appendQueryParameter(PARAM_KEY, key).build();
         URL url = null;
         try{
             url = new URL(buildUri.toString());
+            Log.d(TAG,"URL: " + url);
         }catch (MalformedURLException e){
             e.printStackTrace();
         }
@@ -33,7 +42,7 @@ public class NetworkUtils extends MainActivity {
     }
 
 
-    public static String getResponsedFromHttpUrl(URL url) throws IOException {
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
         try{
             InputStream in = urlConnection.getInputStream();
